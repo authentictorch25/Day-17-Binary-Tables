e4590b185de78e7f7f86f8d3cbfefdb9d3718c2c
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-26 17:33:03 +0530

    [Akash] Added program for binary tree search

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BinaryTableProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+        }
+    }
+}
